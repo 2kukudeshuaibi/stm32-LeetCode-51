9b3249a5ee07afa4aff1ebe407d4978a37ca271b
kukudeshuaibi <18791048236@163.com>
2022-09-09 20:30:30 +0800

    。。。

--- 计算数据/计算数据/源.cpp
+++ 计算数据/计算数据/源.cpp
@@ -3,39 +3,15 @@
 #include <ctype.h>
 
 
-#if 1
-float fun(int m,int n)
+enum
 {
-	int i = 0;
-	int sum1 = 1;
-	int sum2 = 1;
-	int sum3 = 1;
-	float res = 0;
-
-	for (i = 1; i <= m; i++)
-	{
-		sum1 = sum1 * i;
-	}
-	for (i = 1; i <= n; i++)
-	{
-		sum2 = sum2 * i;
-	}
-	for (i = 1; i <= m-n; i++)
-	{
-		sum3 = sum3 * i;
-	}
-	res = sum1 / (sum2 * sum3);
-
-
-
-	return res;
-}
-
-#endif
+	add,
+	abc,
+	ahc
+};
 
 int main()
 {
-	float a = fun(12, 8);
-	printf("%.2f\n", a);
+	printf("%d", abc);
 	return 0;
 }
